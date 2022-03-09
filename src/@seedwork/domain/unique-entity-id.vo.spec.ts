60029f5be78f546c83e31225e9512c38e0ddf6f6
Ridineu de Moraes Neto <ridineu@hotmail.com>
2022-03-09 02:05:31 -0300

    Implementado entidades categoria

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,25 @@
+import InvalidUuidError from "../errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id.vo";
+import { validate as uuidValidate } from "uuid";
+
+describe("UniqueEntityId Unit Test", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+
+  it("should throw error when uuid is invalid", () => {
+    expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const uuid = "5059ee4c-c411-4044-98e0-b3c935fe6451";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+  });
+
+  it("should return a valid uuid when constructor is empty", () => {
+    const vo = new UniqueEntityId();
+    expect(uuidValidate(vo.id)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+  });
+});
---
+++ src/category/domain/entities/category.ts
@@ -0,0 +1,44 @@
+import UniqueEntityId from '../../../@seedwork/domain/unique-entity-id.vo'
+
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export class Category {
+
+  public readonly id: UniqueEntityId;
+
+  constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    this.id = id || new UniqueEntityId();
+    this.description = this.props.description;
+    this.is_active = this.props.is_active;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get description(): string | undefined {
+    return this.props.description;
+  }
+
+  private set description(description: string) {
+    this.props.description = description ?? null;
+  }
+
+  get is_active(): boolean | undefined {
+    return this.props.is_active;
+  }
+
+  private set is_active(is_active: boolean) {
+    this.props.is_active = is_active ?? true;
+  }
+
+  get created_at(): Date | undefined {
+    return this.props.created_at;
+  }
+}
